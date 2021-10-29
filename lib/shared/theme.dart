1f87e6830cca196cbd29709bcfb330863d6a6d65
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2021-10-29 10:49:21 +0700

    Add fonts, themes

---
+++ lib/shared/theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+// noted Global Colors
+Color ckBlue = const Color(0xFF2B2E4A);
+Color cRed = const Color(0xFFE84545);
+Color cPurple = const Color(0xFF903749);
+Color cDarkPurple = const Color(0xFF53354A);
+Color cWhite = const Color(0xFFFFFFFF);
+Color cLightWhite = const Color(0XFFF2F2F2);
+Color cGrey = const Color(0xFF8E8E93);
+Color cLightGrey = const Color(0xFFCFCFCF);
+Color cDarkGrey = const Color(0xFF424242);
+Color cBlack = const Color(0xFF000000);
+
+// Noted fonts
+String poppins = "OpenSans";
+
+// Noted Global TextStyle
+TextStyle tRed = TextStyle(color: cRed, fontFamily: poppins);
+TextStyle tGrey = TextStyle(color: cGrey, fontFamily: poppins);
+TextStyle tDarkGrey = TextStyle(color: cDarkGrey, fontFamily: poppins);
+
+// Noted GlobalFontWeight
+FontWeight light = FontWeight.w300;
+FontWeight normal = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
