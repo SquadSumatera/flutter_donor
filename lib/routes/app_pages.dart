eb5df74cb7c18f9fb068204d272805151b5dd827
Catyousha <jakaasha@gmail.com>
2021-11-04 09:10:01 +0700

    add route & app initialization

---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  static const initial = Routes.home;
+  static final routes = [
+    GetPage(
+      name: Routes.home,
+      page: () => const Scaffold(),
+    ),
+    GetPage(
+      name: Routes.login,
+      page: () => const Scaffold(),
+    ),
+    GetPage(
+      name: Routes.profile,
+      page: () => const Scaffold(),
+    ),
+  ];
+}
---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,7 @@
+part of 'app_pages.dart';
+
+abstract class Routes {
+  static const String home = '/home';
+  static const String login = '/login';
+  static const String profile = '/profile';
+}
