d911c9159b2ab87ab6a5aa4e0d8e626d17b461bc
Catyousha <jakaasha@gmail.com>
2021-11-04 08:52:52 +0700

    sesuaikan theme dengan figma

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,39 +1,49 @@
 import 'package:flutter/material.dart';
 
-// noted Global Colors
-Color ckBlue = const Color(0xFF2B2E4A);
-Color cRed = const Color(0xFFE84545);
-Color cPurple = const Color(0xFF903749);
-Color cDarkPurple = const Color(0xFF53354A);
-Color cWhite = const Color(0xFFFFFFFF);
-Color cLightWhite = const Color(0XFFF2F2F2);
-Color cGrey = const Color(0xFF8E8E93);
-Color cLightGrey = const Color(0xFFCFCFCF);
-Color cDarkGrey = const Color(0xFF424242);
-Color cBlack = const Color(0xFF000000);
+class AppColor {
+  // usage: AppColor.richBlack
+  static const Color richBlack = Color(0xFF0B090A);
+  static const Color bloodRed = Color(0xFF660708);
+  static const Color eerieBlack = Color(0xFF161A1D);
+  static const Color rubyRed = Color(0xFF161A1D);
+  static const Color carnelian = Color(0xFFBA181B);
+  static const Color imperialRed = Color(0xFFDC2626);
+  static const Color silverChalice = Color(0xFFB1A7A6);
+  static const Color lightGrey = Color(0xFFD3D3D3);
+  static const Color cultured = Color(0xFFF5F3F4);
+  static const Color white = Color(0xFFFFFFFF);
+}
 
-// Noted fonts
-String fontFamily = "OpenSans";
+class AppText {
+  static const String fontFamily = "OpenSans";
 
-// Noted Global TextStyle
-TextStyle tRed = TextStyle(
-  color: cRed,
-  fontFamily: fontFamily,
-);
-TextStyle tGrey = TextStyle(
-  color: cGrey,
-  fontFamily: fontFamily,
-);
-TextStyle tDarkGrey = TextStyle(
-  color: cDarkGrey,
-  fontFamily: fontFamily,
-);
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight normal = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+  static const FontWeight extraBold = FontWeight.w800;
+  static const FontWeight black = FontWeight.w900;
 
-// Noted GlobalFontWeight
-FontWeight light = FontWeight.w300;
-FontWeight normal = FontWeight.w400;
-FontWeight medium = FontWeight.w500;
-FontWeight semiBold = FontWeight.w600;
-FontWeight bold = FontWeight.w700;
-FontWeight extraBold = FontWeight.w800;
-FontWeight black = FontWeight.w900;
+  // usage: Text("lipsum", style: AppText.red)
+  static const TextStyle textExtraSmall = TextStyle(
+    fontFamily: fontFamily,
+    fontSize: 8,
+  );
+  static const TextStyle textSmall = TextStyle(
+    fontFamily: fontFamily,
+    fontSize: 10,
+  );
+  static const TextStyle textNormal = TextStyle(
+    fontFamily: fontFamily,
+    fontSize: 13,
+  );
+  static const TextStyle textMedium = TextStyle(
+    fontFamily: fontFamily,
+    fontSize: 15,
+  );
+  static const TextStyle textLarge = TextStyle(
+    fontFamily: fontFamily,
+    fontSize: 24,
+  );
+}
