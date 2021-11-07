eb7eeecc7287d7f4d6c366e5a0d124b961d2dab1
nando <muhammad.rezki.ananda@gmail.com>
2021-11-07 07:16:20 +0700

    Change status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 import 'routes/app_pages.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.black, // navigation bar color
+      statusBarColor: Colors.white, // status bar
+      statusBarIconBrightness: Brightness.dark // color
+      ));
   runApp(const DorplasApp());
 }
 
 class DorplasApp extends StatelessWidget {
-  const DorplasApp({ Key? key }) : super(key: key);
+  const DorplasApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
