13c1b6fec7a69b0d8e268b935bf4001e7fbbe2a6
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2021-11-08 15:05:57 +0700

    Sync Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 import 'routes/app_pages.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.black, // navigation bar color
+      statusBarColor: Colors.white, // status bar
+      statusBarIconBrightness: Brightness.dark // color
+      ));
   runApp(const DorplasApp());
 }
 
