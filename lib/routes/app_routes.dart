ad59b59fdef4a1615a2100985acd556ad9e83956
Catyousha <jakaasha@gmail.com>
2021-11-18 06:43:35 +0700

    duh

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -6,7 +6,7 @@ abstract class Routes {
   static const String home = '/home';
   static const String login = '/login';
   static const String profile = '/profile';
-  static const String profileEditIdentity = 'identity';
+  static const String profileEditIdentity = '/identity';
   static const String location = '/location';
   static const String showMaps = '/showMaps';
   static const String locationSearch = '/locationSearch';
