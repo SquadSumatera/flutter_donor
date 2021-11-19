bf85cfc6c1800054d59563a9536ae28772462675
nando <muhammad.rezki.ananda@gmail.com>
2021-11-19 15:18:15 +0700

    Getting data list institution

--- lib/ui/donor/donor_page.dart
+++ lib/ui/donor/donor_page.dart
@@ -2,6 +2,8 @@ import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_donor/get_x/state/login_getx.dart';
+import 'package:flutter_donor/services/institution_services.dart';
 import 'package:flutter_donor/shared/theme.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
@@ -15,116 +17,124 @@ class DonorPage extends StatefulWidget {
 
 class _DonorPageState extends State<DonorPage> {
   TextEditingController dateController = TextEditingController();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: SvgPicture.asset(
-            'assets/vector/ic_back.svg',
-            width: 12,
+
+  final LoginGetX loginGetX = Get.find<LoginGetX>();
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: SvgPicture.asset(
+              'assets/vector/ic_back.svg',
+              width: 12,
+            ),
+            onPressed: () {
+              Get.back();
+            },
           ),
-          onPressed: () {
-            Get.back();
-          },
+          title: Text(
+            'Pendonor',
+            style: AppText.textMedium.copyWith(fontWeight: AppText.semiBold),
+          ),
+          titleSpacing: 0,
+          backgroundColor: AppColor.cRed,
+          elevation: 0,
         ),
-        title: Text(
-          'Pendonor',
-          style: AppText.textMedium.copyWith(fontWeight: AppText.semiBold),
-        ),
-        titleSpacing: 0,
-        backgroundColor: AppColor.cRed,
-        elevation: 0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Jadwal Pendonor Darah',
-                  style: AppText.textSemiLarge.copyWith(
-                      color: AppColor.cDarkBlue, fontWeight: AppText.semiBold),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 24),
-                  child: Text(
-                    'Lokasi Pendonor',
-                    style: AppText.textMedium.copyWith(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Jadwal Pendonor Darah',
+                    style: AppText.textSemiLarge.copyWith(
                         color: AppColor.cDarkBlue,
                         fontWeight: AppText.semiBold),
                   ),
-                ),
-                SizedBox(
-                  width: 300,
-                  child: DropdownSearch<String>(
-                    showSearchBox: true,
-                    mode: Mode.BOTTOM_SHEET,
-                    showSelectedItems: true,
-                    items: ["Brazil", "Italia", "Tunisia", 'Canada'],
-                    onChanged: print,
-                    dropdownSearchBaseStyle: TextStyle(color: AppColor.rubyRed),
-                    dropdownSearchDecoration: InputDecoration(
-                      hintText: "Pilih Lokasi Pendonoran Terdekat",
-                      hintStyle: AppText.textMedium.copyWith(
-                          color: AppColor.cGrey, fontWeight: AppText.normal),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 24),
+                    child: Text(
+                      'Lokasi Pendonor',
+                      style: AppText.textMedium.copyWith(
+                          color: AppColor.cDarkBlue,
+                          fontWeight: AppText.semiBold),
                     ),
                   ),
-                ),
-                Container(
-                  width: 300,
-                  padding: EdgeInsets.only(top: 12),
-                  child: TextField(
-                    controller: dateController,
-                    showCursor: true,
-                    readOnly: true,
-                    decoration: InputDecoration(
-                      hintText: "Tanggal",
-                      border: const UnderlineInputBorder(),
-                      focusedBorder: const UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue),
-                      ),
-                      enabledBorder: const UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey),
-                      ),
-                      hintStyle: AppText.textMedium.copyWith(
-                          color: AppColor.cGrey, fontWeight: AppText.normal),
-                    ),
-                    onTap: () {
-                      showDatePicker(
-                              context: context,
-                              initialDate: DateTime.now(),
-                              firstDate: DateTime(1945),
-                              lastDate: DateTime.now())
-                          .then((date) {
-                        setState(() {
-                          dateController.text =
-                              "${date!.day}-${date.weekday}-${date.year}";
-                        });
-                      });
-                    },
-                  ),
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                Center(
-                  child: SizedBox(
+                  SizedBox(
                     width: 300,
-                    child:
-                        ElevatedButton(child: Text('Kirim'), onPressed: () {}),
+                      child: DropdownSearch<String?>(
+                      showSearchBox: true,
+                      mode: Mode.BOTTOM_SHEET,
+                      showSelectedItems: false,
+                      onFind: (String? filter) async {
+                        var a = await InstitutionServices.listInstitution(token: loginGetX.token.value);
+                        return a.map((e) => e!.nameInstitutions).toList();
+                      },
+                      onChanged: print,
+                      dropdownSearchBaseStyle: TextStyle(
+                          color: AppColor.rubyRed),
+                      dropdownSearchDecoration: InputDecoration(
+                        hintText: "Pilih Lokasi Pendonoran Terdekat",
+                        hintStyle: AppText.textMedium.copyWith(
+                            color: AppColor.cGrey, fontWeight: AppText.normal),
+                      ),
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  Container(
+                    width: 300,
+                    padding: EdgeInsets.only(top: 12),
+                    child: TextField(
+                      controller: dateController,
+                      showCursor: true,
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        hintText: "Tanggal",
+                        border: const UnderlineInputBorder(),
+                        focusedBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue),
+                        ),
+                        enabledBorder: const UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                        hintStyle: AppText.textMedium.copyWith(
+                            color: AppColor.cGrey, fontWeight: AppText.normal),
+                      ),
+                      onTap: () {
+                        showDatePicker(
+                            context: context,
+                            initialDate: DateTime.now(),
+                            firstDate: DateTime(1945),
+                            lastDate: DateTime.now())
+                            .then((date) {
+                          setState(() {
+                            dateController.text =
+                            "${date!.day}-${date.weekday}-${date.year}";
+                          });
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Center(
+                    child: SizedBox(
+                      width: 300,
+                      child:
+                      ElevatedButton(child: Text('Kirim'), onPressed: () {}),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
-}
