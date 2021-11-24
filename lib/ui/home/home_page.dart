a34b73aea5347d1e5a5166a337b22b0b2e30fe79
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2021-11-24 19:11:08 +0700

    Update Home Page - Added Check Connection and Update Profile Image in Dashboard connect with server

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_donor/get_x/state/check_connection_getx.dart';
 import 'package:flutter_donor/get_x/state/login_getx.dart';
+import 'package:flutter_donor/shared/theme.dart';
 import 'package:flutter_donor/ui/home/home_page_widget.dart';
 import 'package:get/get.dart';
 
@@ -13,10 +14,35 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          banner(),
-        ],
+      body: Obx(
+        () => checkConnectionGetX.status.value == StatusConnection.loading
+            ? const Center(
+                child: CircularProgressIndicator(
+                  color: AppColor.cBlack,
+                  strokeWidth: 5,
+                ),
+              )
+            : checkConnectionGetX.status.value == StatusConnection.failed
+                ? Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                            "Terjadi Kesalahan, coba beberapa saat kembali."),
+                        TextButton(
+                          onPressed: () {
+                            checkConnectionGetX.onInit();
+                          },
+                          child: const Text("Coba Lagi"),
+                        ),
+                      ],
+                    ),
+                  )
+                : ListView(
+                    children: [
+                      banner(checkConnectionGetX),
+                    ],
+                  ),
       ),
     );
   }
