3fe82f944f20ad94f65b2648b8aecb858dc621c3
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2021-11-26 17:11:03 +0700

    Problem Solved Maps Bottom Sheet - Req Rezki

--- lib/ui/location/location_maps_page.dart
+++ lib/ui/location/location_maps_page.dart
@@ -23,37 +23,55 @@ class LocationMapsPage extends StatelessWidget {
         children: [
           LocationMapsMarkerWidget(),
           DraggableScrollableSheet(
-              initialChildSize: 0.25,
-              builder: (_, scrollController) {
-                return Container(
-                  color: Colors.transparent,
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(40.0),
-                          topRight: Radius.circular(40.0),
+            initialChildSize: 0.25,
+            builder: (_, scrollController) {
+              return Container(
+                color: Colors.transparent,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(40.0),
+                        topRight: Radius.circular(40.0),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: const Offset(0, 3),
+                        )
+                      ]),
+                  child: ListView(
+                    children: [
+                      Text(
+                        "Ini Search",
+                        style: AppText.textLarge.copyWith(
+                          color: AppColor.cRed,
+                          fontWeight: AppText.bold,
                         ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: const Offset(0, 3),
-                          )
-                        ]),
-                    child: ListView.builder(
-                      controller: scrollController,
-                      itemCount: 25,
-                      itemBuilder: (BuildContext context, int index) {
-                        return LocationListWidget(
-                          index: index,
-                        );
-                      },
-                    ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(
+                        height: 4.0,
+                      ),
+                      ListView.builder(
+                        primary: false,
+                        shrinkWrap: true,
+                        controller: scrollController,
+                        itemCount: 25,
+                        itemBuilder: (BuildContext context, int index) {
+                          return LocationListWidget(
+                            index: index,
+                          );
+                        },
+                      ),
+                    ],
                   ),
-                );
-              })
+                ),
+              );
+            },
+          )
         ],
       ),
     );
