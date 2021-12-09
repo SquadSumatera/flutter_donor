00c7cebf6006ca16d10f2e3adc537a32f2fd209a
Catyousha <jakaasha@gmail.com>
2021-12-09 15:04:03 +0700

    typo homedivier

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -61,7 +61,7 @@ class HomePage extends StatelessWidget {
                               : stokPlasma(
                                   donorStatisticController.dataSubmission.value,
                                 ),
-                      homeDivier(),
+                      homeDivider(),
                       donorStatisticController.statusStatistic.value ==
                               StatusStatisticDonor.loading
                           ? onLoad()
@@ -71,7 +71,7 @@ class HomePage extends StatelessWidget {
                                   donorStatisticController.onInit();
                                 })
                               : trend(donorStatisticController.dataList),
-                      homeDivier(),
+                      homeDivider(),
                       title("Jadwal Donor"),
                       Obx(
                         () => donorHistoryController.status.value ==
